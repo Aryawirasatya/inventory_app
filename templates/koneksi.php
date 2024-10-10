4ddeabc0b6fbac53398bbac4073c15342e1aebb5
arya wirasatya <aryawirasatya39@gmail.com>
2024-10-10 12:41:34 +0700

    inventory app

---
+++ templates/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+define('HOST_NAME','localhost');
+define('USER_NAME','ROOT');
+define('PASSWORD','');
+define('DB_NAME','inventory_app');
+
+$koneksi = mysqli_connect(HOST_NAME,USER_NAME,PASSWORD,DB_NAME);
